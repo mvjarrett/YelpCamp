293d0f27c15be92c38fa521728821ba1f05b0546
CJ Wood <cjwood0@gmail.com>
2020-04-15 00:15:22 -0400

    CRUD order

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,25 +4,19 @@ var Campground = require('../models/campground');
 var checkAuth = require('../middleware/check-auth');
 var CampgroundController = require('../controllers/campground');
 
-//INDEX ROUTE - show all campgrounds
-router.get('/', CampgroundController.showCampgrounds);
-
-//CREATE ROUTE - add new campground to db
+//create
 router.post('/', checkAuth.isLoggedIn, CampgroundController.createCampground);
-
-//NEW ROUTE - show form to create new campgrounds
 router.get('/new', checkAuth.isLoggedIn, CampgroundController.newCampground);
 
-//SHOW ROUTE - shows more info about one campground
+//read
+router.get('/', CampgroundController.showCampgrounds);
 router.get('/:id', CampgroundController.showCampground);
 
-//EDIT campground route
+//update
 router.get('/:id/edit', checkAuth.checkCampOwner, CampgroundController.editCampground);
-//UPDATE campground route
 router.put('/:id', checkAuth.checkCampOwner, CampgroundController.updateCampground);
 
-// DESTROY CAMPGROUND ROUTE
-
+//delete
 router.delete('/:id', checkAuth.checkCampOwner, CampgroundController.destroyCampground);
 
 module.exports = router;
