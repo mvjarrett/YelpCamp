c3b14273024deb36d54318e6bc3136d53d363cab
CJ Wood <cjwood0@gmail.com>
2020-04-16 01:51:15 -0400

    [WIP] Start refactoring models

--- models/campground.js
+++ models/campground.js
@@ -4,19 +4,8 @@ var campgroundSchema = new mongoose.Schema({
 	name: String,
 	image: String,
 	description: String,
-	author: {
-		id: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User'
-		},
-		username: String
-	},
-	comments: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Comment'
-		}
-	]
+	author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment'	}]
 });
 
 module.exports = mongoose.model('campground', campgroundSchema);
--- models/comment.js
+++ models/comment.js
@@ -2,13 +2,7 @@ var mongoose = require('mongoose');
 
 var commentSchema = mongoose.Schema({
 	text: String,
-	author: {
-		id: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User'
-		},
-		username: String
-	}
+	author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
